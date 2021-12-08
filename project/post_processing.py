1eadbcc39294e8a16cf92c35a1665b047f9a0b5a
Radian Gondokaryono <radian.gondokaryono@mail.utoronto.ca>
2021-12-08 14:46:41 -0500

    post_processing.py, use a plotter class for each figure, and subplots

--- project/post_processing.py
+++ project/post_processing.py
@@ -1,13 +1,23 @@
 import matplotlib.pyplot as plt
 
-def plot_2d_y(X, y, label = ""):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.scatter(X[:, 0], X[:, 1], y)
-    ax.title.set_text(label)
+class Plotter:
+    def __init__(self, rows, cols):
+        self.fig = plt.figure()
+        standard_size_in = 5
+        self.fig.set_figheight(standard_size_in * rows)
+        self.fig.set_figwidth(standard_size_in * cols)
+        self.rows = rows
+        self.cols = cols
+        self.idx = 1
 
-def plot_2d(X, label = ""):
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
-    ax.scatter(X[:, 0], X[:, 1])
-    ax.title.set_text(label)
+    def plot_2d_y(self, X, y, label = ""):
+        ax = self.fig.add_subplot(self.rows, self.cols, self.idx, projection='3d')
+        self.idx = self.idx + 1
+        ax.scatter(X[:, 0], X[:, 1], y)
+        ax.title.set_text(label)
+
+    def plot_2d(self, X, label = ""):
+        ax = self.fig.add_subplot(self.rows, self.cols, self.idx, projection='3d')
+        self.idx = self.idx + 1
+        ax.scatter(X[:, 0], X[:, 1])
+        ax.title.set_text(label)
