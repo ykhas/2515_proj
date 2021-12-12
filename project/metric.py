90348bee020b86e8c7037f0d85af9472f49c9f17
Radian Gondokaryono <radian.gondokaryono@mail.utoronto.ca>
2021-12-12 14:31:59 -0500

    Add metrics lib and test

---
+++ project/metric.py
@@ -0,0 +1,13 @@
+import sklearn.metrics
+
+def compute_errors(y_true, y_test):
+    return y_test-y_true
+
+def compute_mean_abs_error(y_true, y_predict):
+    return sklearn.metrics.mean_absolute_error(y_true, y_predict)
+
+def output_performance(y_true, y_predict):
+    print("--- Report ---")
+    print("Mean absolute error: ", compute_mean_abs_error(y_true, y_predict))
+    print("Mean squared error: ", sklearn.metrics.mean_squared_error(y_true, y_predict))
+    print("Max error: ", sklearn.metrics.max_error(y_true, y_predict))
