8892bf45e19a194cc3cdba78bb3e950b3158d9c8
Radian Gondokaryono <radian.gondokaryono@mail.utoronto.ca>
2021-12-12 12:47:03 -0500

    Smaller marker size

--- project/post_processing.py
+++ project/post_processing.py
@@ -5,6 +5,7 @@ class Plotter:
     def __init__(self, rows, cols):
         self.fig = plt.figure()
         standard_size_in = 5
+        self.standard_marker_size = 1
         self.fig.set_figheight(standard_size_in * rows)
         self.fig.set_figwidth(standard_size_in * cols)
         self.rows = rows
@@ -14,13 +15,13 @@ class Plotter:
     def plot_2d_y(self, X, y, label = ""):
         ax = self.fig.add_subplot(self.rows, self.cols, self.idx, projection='3d')
         self.idx = self.idx + 1
-        ax.scatter(X[:, 0], X[:, 1], y)
+        ax.scatter(X[:, 0], X[:, 1], y, s = self.standard_marker_size)
         ax.title.set_text(label)
 
     def plot_2d(self, X, label = ""):
         ax = self.fig.add_subplot(self.rows, self.cols, self.idx, projection='3d')
         self.idx = self.idx + 1
-        ax.scatter(X[:, 0], X[:, 1])
+        ax.scatter(X[:, 0], X[:, 1], s = self.standard_marker_size)
         ax.title.set_text(label)
 
     def plot_2d_colormesh(self,y , x_dim, t_dim, label = ""):
